ff2b257d34ee3e0a1ee0a6e6d159fbff6077eb4a
TheoGatea <theo.gatea@gmail.com>
2026-02-15 15:46:32 +0100

    added logic to crud operations

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -4,6 +4,48 @@ import six
 from swagger_server.models.student import Student  # noqa: E501
 from swagger_server import util
 
+import os
+import tempfile
+from functools import reduce
+
+from tinydb import TinyDB, Query
+
+db_dir_path = tempfile.gettempdir()
+db_file_path = os.path.join(db_dir_path, "students.json")
+student_db = TinyDB(db_file_path)
+
+
+def add(student=None):
+    queries = []
+    query = Query()
+    queries.append(query.first_name == student.first_name)
+    queries.append(query.last_name == student.last_name)
+    query = reduce(lambda a, b: a & b, queries)
+    res = student_db.search(query)
+    if res:
+        return 'already exists', 409
+
+    doc_id = student_db.insert(student.to_dict())
+    student.student_id = doc_id
+    return student.student_id
+
+
+def get_by_id(student_id=None, subject=None):
+    student = student_db.get(doc_id=int(student_id))
+    if not student:
+        return 'not found', 404
+    student['student_id'] = student_id
+    print(student)
+    return student
+
+
+def delete(student_id=None):
+    student = student_db.get(doc_id=int(student_id))
+    if not student:
+        return 'not found', 404
+    student_db.remove(doc_ids=[int(student_id)])
+    return student_id
+
 
 def add_student(body=None):  # noqa: E501
     """Add a new student
@@ -17,7 +59,8 @@ def add_student(body=None):  # noqa: E501
     """
     if connexion.request.is_json:
         body = Student.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        return add(body)
+    return 500,'error'
 
 
 def delete_student(student_id):  # noqa: E501
@@ -30,7 +73,7 @@ def delete_student(student_id):  # noqa: E501
 
     :rtype: object
     """
-    return 'do some magic!'
+    return delete(student_id)
 
 
 def get_student_by_id(student_id):  # noqa: E501
@@ -43,4 +86,4 @@ def get_student_by_id(student_id):  # noqa: E501
 
     :rtype: Student
     """
-    return 'do some magic!'
+    return get_by_id(student_id)
